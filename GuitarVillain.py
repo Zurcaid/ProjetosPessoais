820df9cf37d9854df759c954fed6d5591abafa1f
Zurcaid <zurcaid.z@gmail.com>
2025-04-09 16:13:16 -0300

    Update GuitarVillain.py

--- GuitarVillain.py
+++ GuitarVillain.py
@@ -6,6 +6,11 @@ from pynput import keyboard
 pont = []
 teclas = []
 
+b2 = [2, 3, 6, 7, 10, 11, 14, 18, 19, 22, 26]
+b3 = [4, 5, 6, 7, 12, 13, 14, 20, 21, 22, 28]
+b4 = [8, 9, 10, 11, 12, 13, 14, 24, 25, 26, 28]
+b5 = [16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 28]
+
 def TeclaPressionada(key):
 	if(key.char != 'o'):
 		if key.char not in teclas:
@@ -13,35 +18,35 @@ def TeclaPressionada(key):
 	else:
 		for j in teclas:
 			if(j == '1'):
-				if(musicaConfig[screen-9] == 1):
+				if(musicaConfig[screen-9]%2 != 0) or (musicaConfig[screen-8]%2 != 0) or (musicaConfig[screen-10]%2 != 0):
 					pont.append(0)
 					print("ACERTOU!")
 				else:
 					pont.pop()
 					print("ERROU!")
 			elif(j == '2'):
-				if(musicaConfig[screen-9] == 2):
+				if(musicaConfig[screen-9] in b2) or (musicaConfig[screen-8] in b2) or (musicaConfig[screen-10] in b2):
 					pont.append(0)
 					print("ACERTOU!")
 				else:
 					pont.pop()
 					print("ERROU!")
 			elif(j == '3'):
-				if(musicaConfig[screen-9] == 4):
+				if(musicaConfig[screen-9] in b3) or (musicaConfig[screen-8] in b3) or (musicaConfig[screen-10] in b3):
 					pont.append(0)
 					print("ACERTOU!")
 				else:
 					pont.pop()
 					print("ERROU!")
 			elif(j == '4'):
-				if(musicaConfig[screen-9] == 8):
+				if(musicaConfig[screen-9] in b4) or (musicaConfig[screen-8] in b4) or (musicaConfig[screen-10] in b4):
 					pont.append(0)
 					print("ACERTOU!")
 				else:
 					pont.pop()
 					print("ERROU!")
 			elif(j == '5'):
-				if(musicaConfig[screen-9] == 16):
+				if(musicaConfig[screen-9] in b5) or (musicaConfig[screen-8] in b5) or (musicaConfig[screen-10] in b5):
 					pont.append(0)
 					print("ACERTOU!")
 				else:
@@ -68,12 +73,52 @@ for i in musicaConfig: # Conversao dos valores pras teclas
 		musica.append("|X| | | | |")
 	elif i == 2:
 		musica.append("| |X| | | |")
+	elif i == 3:
+		musica.append("|X|X| | | |")
 	elif i == 4:
 		musica.append("| | |X| | |")
+	elif i == 5:
+		musica.append("|X| |X| | |")
+	elif i == 6:
+		musica.append("| |X|X| | |")
+	elif i == 7:
+		musica.append("|X|X|X| | |")
 	elif i == 8:
 		musica.append("| | | |X| |")
+	elif i == 9:
+		musica.append("|X| | |X| |")
+	elif i == 10:
+		musica.append("| |X| |X| |")
+	elif i == 11:
+		musica.append("|X|X| |X| |")
+	elif i == 12:
+		musica.append("| | |X|X| |")
+	elif i == 13:
+		musica.append("|X| |X|X| |")
+	elif i == 14:
+		musica.append("| |X|X|X| |")
 	elif i == 16:
 		musica.append("| | | | |X|")
+	elif i == 17:
+		musica.append("|X| | | |X|")
+	elif i == 18:
+		musica.append("| |X| | |X|")
+	elif i == 19:
+		musica.append("|X|X| | |X|")
+	elif i == 20:
+		musica.append("| | |X| |X|")
+	elif i == 21:
+		musica.append("|X| |X| |X|")
+	elif i == 22:
+		musica.append("| |X|X| |X|")
+	elif i == 24:
+		musica.append("| | | |X|X|")
+	elif i == 25:
+		musica.append("|X| | |X|X|")
+	elif i == 26:
+		musica.append("| |X| |X|X|")
+	elif i == 28:
+		musica.append("| | |X|X|X|")
 
 
 VerificacaoTecla = keyboard.Listener(on_press=TeclaPressionada, on_release=TeclaSolta)
