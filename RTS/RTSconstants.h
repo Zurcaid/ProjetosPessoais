68f7989fe1383581864b0933180ad98534861349
Zurcaid <zurcaid.z@gmail.com>
2025-06-12 17:16:55 -0300

    Update RTSconstants.h

--- RTS/RTSconstants.h
+++ RTS/RTSconstants.h
@@ -8,13 +8,37 @@ int input;
 
 class Civilization{
 	public:
-		int alignment = 0;
-		int xp = 0;
-		int money = 0;
-		int type, king;
+		// Caracteristicas gerais
+		int alignment = 0; // Varia de 0 a 100
+		int xp = 0; // 1 lvl = 100 xp
+	    int tech_lvl = 0;
+		int kind, king;
 		
-		vector<string> troops;
-		vector<int> troopsNum;
+		// Recursos
+		int money = 0;
+		int ores,wood,stone,raw_food; // Primarios
+		int gear,steel,paper,chemicals; // Secundarios
+		
+		// Construcoes
+		vector<int> construcoes;
+		
+		// Populacao
+		int population = 10;
+		int doctor,teacher,farmworker,shopkeeper,worker,extractivist,guard,criminal; // Empregos
+		int happyness = 50; // Varia de 0 a 100
+		float growing_rate = 0.0;
+		
+		// Relacoes diplomaticas
+		string nations_known[];
+		string nk_relationship[];
+		int pacts[];
+		int trade_balance;
+		
+		vector<vector<int>> armys;      // Exercitos
+		vector<vector<int>> army_qnt;   // Quantidade de tropas em cada exercito
+		
+		vector<string> troops;  // Tropas no pais
+		vector<int> troopsNum;  // Quantidade de cada tropa
 		
 		void changeAlignment(int x){
 			alignment += x;
@@ -39,23 +63,25 @@ class Civilization{
 				troopsNum.push_back(n);
 			}
 		}
-		void setType(int x){
-			type = x;
+		void setKind(int x){
+			kind = x;
 		}
 		void setKing(int x){
 			king = x;
 		}
 };
 
-Civilization WarKingdom;
+Civilization Kingdom1;
 
 class King{
 	public:
 		int lvl;
 		int hp = 10;
 		
-		void passive1(){
-			
+		void passive1(int a, int b, int c, int d, int e, int f){
+			int armyDmg_boost = a;
+			int armyHp_boost = b;
+			int moneyGen_boost = c;
 		}
 		void skill1(){
 			
