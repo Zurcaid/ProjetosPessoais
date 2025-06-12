1ec9821befcffcbf5d0f75bada50ba205176431e
Zurcaid <zurcaid.z@gmail.com>
2025-06-12 18:04:29 -0300

    Update RTSfunctions.cpp

--- RTS/RTSfunctions.cpp
+++ RTS/RTSfunctions.cpp
@@ -1,4 +1,38 @@
+#ifndef funcoes
+#define funcoes
+
+void Civilization::changeAlignment(int x){
+	alignment += x;
+}
+void Civilization::changeXp(int x){
+	xp += x;
+}
+		
+void Civilization::setTroops(string x, int n){
+	int size = troops.size();
+	int pos = size+1;
+	for(int i = 0; i <= size; i++){
+		if(troops.at(i) == x){
+			pos = i;
+			break;
+		}
+	}
+    if(pos <= size){
+		troopsNum.at(pos) = troopsNum.at(pos) + n;
+	}else{
+		troops.push_back(x);
+		troopsNum.push_back(n);
+	}
+}
+void Civilization::setKind(int x){
+	kind = x;
+}
+void Civilization::setKing(int x){
+	king = x;
+}
+
 void initCivilizations(){
+    Civilization Kingdom1;
 	Kingdom1.changeAlignment(50);
 	Kingdom1.setKind(0);
 	Kingdom1.setKing(0);
@@ -19,3 +53,5 @@ void loadGame(){
 void playerTurn(){
 	
 }
+
+#endif
