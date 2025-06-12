773e0c785528a71acba0083a98ca6fb1b94f1253
Zurcaid <zurcaid.z@gmail.com>
2025-06-12 12:21:11 -0300

    Atualizar o RTS.cpp

--- RTS/RTS.cpp
+++ RTS/RTS.cpp
@@ -1,1 +1,22 @@
+#include "RTSconstants.h"
+#include "RTSfunctions.cpp"
+using namespace std;
 
+int main()
+{
+	init_game = beginGame();
+	if(init_game == 1){
+		newGame();
+	}else{
+		loadGame();
+	}
+	initCivilizations();
+	
+	while(1){
+		playerTurn();
+		age += 1;
+		cout << "Date: " << age;
+		cin >> input;
+	}
+	return 0;
+}
