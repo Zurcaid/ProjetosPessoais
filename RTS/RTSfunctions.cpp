95b33f46d7509ac5ab2068bef050cb164629e973
Zurcaid <zurcaid.z@gmail.com>
2025-06-12 12:21:32 -0300

    Atualizar o RTSfunctions.cpp

--- RTS/RTSfunctions.cpp
+++ RTS/RTSfunctions.cpp
@@ -1,1 +1,21 @@
+void initCivilizations(){
+	WarKingdom.changeAlignment(50);
+	WarKingdom.setType(0);
+	WarKingdom.setKing(0);
+}
 
+int beginGame(){
+	return 1;
+}
+
+void newGame(){
+	
+}
+
+void loadGame(){
+	
+}
+
+void playerTurn(){
+	
+}
