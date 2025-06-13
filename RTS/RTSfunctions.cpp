dc48edabbc907490d4b64afdf9a8d4f9272b56e1
Zurcaid <zurcaid.z@gmail.com>
2025-06-13 13:03:56 -0300

    Atualizar o RTSfunctions.cpp

--- RTS/RTSfunctions.cpp
+++ RTS/RTSfunctions.cpp
@@ -1,15 +1,10 @@
 #include "RTSconstants.h"
-#include "RTSfunctions.h"
 
 using namespace std;
 
-int init_game;
-int month;
-int year;
-string input;
 // Funcoes relativas ao gerenciamento da cidade
 Civilization::Civilization(int a, int b, int c){
-    Civilization::alignment = a;
+	Civilization::alignment = a;
     Civilization::kind = b;
     Civilization::king = c;
 }
@@ -27,8 +22,8 @@ void Civilization::setTroops(string x, int n){
 	int pos = size+1;
 	for(int i = 0; i <= size; i++){
 		if(troops.at(i) == x){
-			pos = i;
-			break;
+		pos = i;
+		break;
 		}
 	}
     if(pos <= size){
@@ -46,12 +41,57 @@ void Civilization::setKing(int x){
 }
 
 // Funcoes relativas ao andamento do jogo
+// t1=tech_req,ap=aproval,pop=population,a=kind,b=sector,c=kingdom,d=cost,e=money,f=ores/teacher,g=wood/doctor,h=stone/guard,i=raw_food/criminal,j=products,k=gear,l=steel,m=paper,n=chemicals,o=food,p=worker,q=farmworker,r=extractivist,s=shopkeeper,t=troops,u=troopsnum,t2=tech_gen;
+// Setor 1: Estruturas relacionadas ao consumo ou producao de material.
+// Setor 2: Outras estruturas (hospitais, escolas, etc).
+Buildings::Buildings(int t1,int ap,int pop,int a,int b,string c,int d,int e,int g,int h,int i,int j,int k,int l,int m,int n,int o,int p,int q,int r,int s,int t,int u,int t2){
+	Buildings.tech_req = t1;
+	Buildings.aproval = ap;
+	Buildings.population = pop;
+	Buildings.kind = a;
+	Buildings.sector = b;
+	Buildings.kingdom = c;
+	Buildings.cost = d;
+	Buildings.money = e;
+	Buildings.products = j;
+	Buildings.gear = k;
+	Buildings.steel = l;
+	Buildings.paper = m;
+	Buildings.chemicals = n;
+	Buildings.food = o;
+	Buildings.worker = p;
+	Buildings.farmworker = q;
+	Buildings.extractivist = r;
+	Buildings.shopkeeper = s;
+	Buildings.troops = t;
+	Buildings.troopsnum = u;
+	Buildings.tech_gen = t2;
+	if(b==1){
+		Buildings.ores = f;
+		Buildings.wood = g;
+		Buildings.stone = h;
+		Buildings.raw_food = i;
+	}else{
+		Buildings.teacher = f;
+		Buildings.doctor = g;
+		Buildings.guard = h;
+		Buildings.criminal = i;
+	}
+}
+
+
+void Buildings::buildConstruction(Civilization &obj){
+	Civilization.money -= Buildings.cost;
+}
+
+Civilization PlayerKingdom(50,0,0);
 
 void initCivilizations(){
-    Civilization Kingdom1(50, 0, 0);
+	
 }
 
 int beginGame(){
+	
 	return 1;
 }
 
@@ -64,5 +104,5 @@ void loadGame(){
 }
 
 void playerTurn(){
-	cout << Kingdom1.alignment;
+	cout << PlayerKingdom.alignment;
 }
