b063ed247db1d1cc93c356cf9ecb38dfee037f6d
Zurcaid <zurcaid.z@gmail.com>
2025-06-14 12:24:40 -0300

    Atualizar o RTS.cpp

--- RTS/RTS.cpp
+++ RTS/RTS.cpp
@@ -1,31 +1,36 @@
 #include "RTSconstants.h"
 #include "RTSfunctions.h"
+#include "RTSfunctions.cpp"
 #include <iostream>
 using namespace std;
 
-int month,init_game;
+int month, init_game;
 int year = 1;
 string input;
 
 int main()
 {
 	init_game = beginGame();
-	if(init_game == 1){
+	if (init_game == 1)
+	{
 		newGame();
-	}else{
+	}
+	else
+	{
 		loadGame();
 	}
-	initCivilizations();
 
-	while(1){
+	while (1)
+	{
 		playerTurn();
 		month += 1;
-		if(month >= 12){
-    		month = month - 12;
-    		year += 1;
+		if (month >= 13)
+		{
+			month = month - 12;
+			year += 1;
 		}
-	cout << "Date: Month " << month << " | Year " << year;
-	cin >> input;
+		cout << "Date: Month " << month << " | Year " << year << " ... ";
+		cin >> input;
 	}
 	return 0;
 }
