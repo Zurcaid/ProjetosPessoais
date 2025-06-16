41bbc0f583a8cd3046b6574144a9f68218f02a10
Zurcaid <zurcaid.z@gmail.com>
2025-06-16 10:39:14 -0300

    Update RTSfunctions.cpp

--- RTS/RTSfunctions.cpp
+++ RTS/RTSfunctions.cpp
@@ -7,6 +7,38 @@ using namespace std;
 #include <cstdlib>
 
 string input1, input2, input3;
+int difficulty, style;
+string end_of_page = "--------------------\n";
+
+//a=kingdom,b=age,c=max_age,d=hp,e=dmg,f=alignment,g=troop_cacapity,h=name;
+King KingArthur(0,20,90,300,300,75,10000,"The Prophesied King"), Gilgamesh(0,20,130,500,500,50,0,"The King of All that Exists"), MilitaryGenius(0,30,90,100,100,50,200000,"The Military Genius");
+King GodEmperor(1,30,500,300,300,50,10000,"The God Emperor of Humanity"), ChosenOne(1,16,70,500,500,100,1000,"The Chosen One"), FalseFalconOfLight(1,20,80,100,100,0,100,"The False Falcon of Light");
+King NamelessGodOfEvil(2,7000,50000,100,100,0,0,"The Nameless God Of Evil"), VampireLord(2,120,10000,500,500,0,10000,"The Vampire Lord"), ReincarnatedDemonKing(2,10,1200,500,500,50,10000,"The Reincarnated Demon King");
+King WorldEater(3,7000,50000,1000,1000,0,0,"The World Eater"), PerfectBeing(3,7000,50000,1000,1000,50,0,"The Perfect Being"), KingOverHeaven(3,110,50000,750,750,50,10000,"The Ascended King Over Heaven");
+King MonarchOfIllusions(4,3000,10000,1000,100,0,10,"The Monarch of Illusions"), KingOfShadows(4,20,600,500,500,50,1000,"The King of Shadows"), StrongestSorcerer(4,30,90,750,750,50,0,"The Strongest Sorcerer");
+King SithLord(5,80,1000,200,200,0,0,"The Senate Lord"), Android(5,1,100,200,200,50,10000,"The Android");
+King IA(6,1,2000,100,100,50,0,"The IA"), GhoulEmperor(6,20,100,500,500,50,10000,"The Ghoul Emperor"), ManInTheSuit(6,40,200,100,100,0,0,"TheManInTheSuit"), PerfectHomunculus(6,1,150,500,500,0,100,"The perfect homunculus");
+
+// a=kingdom,b=kind,c=ranged,d=food_cost_per_month,e=dmg,f=hp,g=size,h=name
+Troops Knight_1(0,1,0,1,10,10,2,"Training knight"), Archer_1(0,2,1,1,5,5,2,"Training archer"), Shielder_1(0,3,0,2,5,18,3,"Weak shielder"), Horse_1(0,4,0,3,10,18,5,"Horse mounted training knight");
+Troops Cleric_1(1,1,0,1,8,12,2, "Cleric student"), HolyArcher_1(1,2,1,1,7,3,2,"Training holy archer"), Paladin_1(1,3,0,2,4,23,3,"Paladin's disciple"), Falcon_1(1,4,1,1,5,5,1,"Young falcon");
+Troops Goblin_1(2,1,0,0,15,7,2,"Young goblin"), RangedGoblin_1(2,2,1,2,10,7,2,"Rock thrower goblin"), Slime_1(2,3,0,0,1,25,2,"Small slime"), Bat_1(2,4,1,1,3,3,1,"Bat");
+Troops Believer_1(3,1,0,1,8,8,2,"Novice believer"), ArcherBeliever_1(3,2,1,1,5,5,2,"Novice believer with bow"), Penitent_1(3,3,0,0,2,25,2,"Weak penitent"), ElementalSpirit_1(3,4,0,0,10,10,1,"Young elemental spirit");
+Troops Summon_1(4,1,0,0,8,8,2,"Weak summoned clone"), Mage_1(4,2,1,2,10,3,2,"Mage Apprentice"), Caster_1(4,3,0,2,0,30,3,"Apprentice shield caster"), WolfSummon_1(4,4,0,0,10,5,1,"Weak summoned wolf");
+Troops Droid_1(5,1,0,0,6,6,1,"Small battle droid"), RangedDroid_1(5,2,1,0,6,3,1,"Small ranged droid"), Barrier_1(5,3,0,0,0,30,3,"Weak mobile barrier"), Drone_1(5,4,1,0,10,1,1,"Weak combat drone");
+Troops Parasite_1(6,1,0,0,10,1,1,"Small weak parasite"), Bacteria_1(6,2,1,2,1,1,0,"Weak air propagant bacteria colony"), Flesh_1(6,3,0,0,2,20,2,"Weak flesh wall"), ZombieWolf_1(6,4,0,1,10,5,1,"Zombie dog");
+
+// a=kingdom,b=capacity,c=loyalty,d=xp,e=dmg,f=hp,g=archery_boost,h=melee_boost,i=defender_boost,j=beast_boost,k=dmg_boost,l=hp_boost,m=foodcost_boost,n=moral_boost,o=alignment, p=name;
+Champions MiyamotoMusashi(0,30,90,0,100,100,250,250,100,80,200,100,80,150,50,"Miyamoto Musashi"), Spartacus(0,12000,90,0,150,200,90,105,105,90,100,100,70,130,50,"Spartacus");
+Champions JonathanJoestar(1,10,80,0,200,200,110,150,150,150,150,200,100,200,100,"Jonathan Joestar"), FatherMozgus(1,1000,80,0,30,200,100,100,120,90,90,120,80,150,30,"Father Mozgus");
+Champions PyramidHead(2,100,100,0,200,200,100,150,200,150,100,150,0,100,0,"Pyramid Head");
+Champions Indra(3,500,60,0,150,150,100,110,110,200,110,120,100,110,50,"Indra Otsukitsuki");
+Champions Miraak(4,1000,60,0,200,200,110,95,95,110,105,100,100,100,50,"Miraak");
+Champions Ghostrunner(5,100,100,0,300,100,100,300,70,80,150,80,100,150,50,"Ghostrunner");
+Champions Hunter(6,10000,100,0,100,500,80,105,110,110,90,110,100,200,0,"Hunter");
+
+// t1=tech_req,ap=aproval,pop=population,a=kind,b=sector,c=kingdom,d=cost,e=money,f=ores/teacher,g=wood/doctor,h=stone/guard,i=raw_food/criminal,j=products,k=gear,l=steel,m=paper,n=chemicals,o=food,p=worker,q=farmworker,r=extractivist,s=shopkeeper,t=troops,u=troopsnum,t2=tech_gen,wc=wood_cost,stone_c=stone_cost,steel_c=steel_cost;
+Buildings FruitFarm1(0, 1.0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 3, 2, 0, "Small animal farm"), AnimalFarm1(0, 1.0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 3, 2, 0, "Small fruit farm");
 
 vector<vector<King>> kings = {{KingArthur,Gilgamesh,MilitaryGenius}, {GodEmperor,ChosenOne,FalseFalconOfLight}, {NamelessGodOfEvil,VampireLord,ReincarnatedDemonKing}, {WorldEater,PerfectBeing,KingOverHeaven}, {MonarchOfIllusions,KingOfShadows,StrongestSorcerer}, {SithLord,Android}, {IA,GhoulEmperor,ManInTheSuit,PerfectHomunculus}};
 
@@ -15,8 +47,6 @@ vector<vector<Champions>> commanders = {{MiyamotoMusashi,Spartacus}, {JonathanJo
 vector<vector<Troops>> soldier= {{Knight_1,Archer_1,Shielder_1,Horse_1}, {Cleric_1,HolyArcher_1,Paladin_1,Falcon_1}, {Goblin_1,RangedGoblin_1,Slime_1,Bat_1}, {Believer_1,ArcherBeliever_1,Penitent_1,ElementalSpirit_1}, {Summon_1,Mage_1,Caster_1,WolfSummon_1}, {Droid_1, RangedDroid_1,Barrier_1,Drone_1}, {Parasite_1,Bacteria_1,Flesh_1,ZombieWolf_1}};
 
 vector<vector<Buildings>> constructions = {{FruitFarm1, AnimalFarm1}, {FruitFarm1, AnimalFarm1}};
-
-
 
 // Funcoes relativas ao gerenciamento da cidade
 Civilization::Civilization(int a, int b, int c, int d)
@@ -129,7 +159,7 @@ void Civilization::monthlyUpdates(){
 // Funcoes relativas ao gerenciamento de reis
 //a=kingdom,b=age,c=max_age,d=hp,e=dmg,f=alignment,g=troop_capacity,h=name;
 King::King(int a, int b, int c, int d, int e, int f, int g, string h){
-	name = g;
+	name = h;
 	kingdom = a;
 	age = b;
 	max_age = c;
@@ -378,21 +408,42 @@ void newGame()
 			PlayerKingdom.civilization_name = input1;
 		}
 	}
-	int king_selected = 0;
+	int king_selected = 100;
 	int kind_selected;
+	int inputnumber;
 	if (style == 2){
-		while(!king_selected){
+		while(king_selected == 100){
 			cout << end_of_page;
-			cout << "Choose a civilization kind:\n0 - Human: An empire based on humanity's history, historical warriors, legends.\n1 - Holy: A holy empire whose objective is to spread the teachings of God to the world.\n2 - Evil: An empire of darkness whose objective is to control the world, destroying those who oppose it.\n3 - Deity: An empire controled by godlike beings.\n4 - Witchcraft: An empire of sorcerers who use techniques to surpass their limits as mortal beings.\n5 - Technology: The remains of an advanced empire, now with the task of advance even more the technology to control the world.\n6 - Biotechnology: Founded by unethical experiments on living beings, this is an empire that will create monsters to control the world.\nChoose one to see the types of king you can select: ";
+			cout << "Choose a civilization kind:\n0 - Human\n1 - Holy\n2 - Evil\n3 - Deity\n4 - Witchcraft\n5 - Technology\n6 - Biotechnology\nChoose one to see the types of king you can select: ";
 			while(!(cin >> kind_selected)){
 				cout << "No option selected, choose again.\n";
 				cout << end_of_page;
-				cout << "Choose a civilization kind:\n0 - Human: An empire based on humanity's history, historical warriors, legends.\n1 - Holy: A holy empire whose objective is to spread the teachings of God to the world.\n2 - Evil: An empire of darkness whose objective is to control the world, destroying those who oppose it.\n3 - Deity: An empire controled by godlike beings.\n4 - Witchcraft: An empire of sorcerers who use techniques to surpass their limits as mortal beings.\n5 - Technology: The remains of an advanced empire, now with the task of advance even more the technology to control the world.\n6 - Biotechnology: Founded by unethical experiments on living beings, this is an empire that will create monsters to control the world.\nChoose one to see the types of king you can select: ";
+		    	cout << "Choose a civilization kind:\n0 - Human\n1 - Holy\n2 - Evil\n3 - Deity\n4 - Witchcraft\n5 - Technology\n6 - Biotechnology\nChoose one to see the types of king you can select: ";
 				cin.clear();
 				cin.ignore(10000, '\n');
 			}
 			if((kind_selected>=0) and (kind_selected<=6)){
-				cout << "Good";
+			    vector<King> kingdom_kind = kings.at(kind_selected);
+			    int size = kingdom_kind.size();
+			    cout << "Available title options available for this kind of kingdom:\n";
+			    for(int i = 0; i < size; i++){
+			        King king_kind = kingdom_kind.at(i);
+			        cout << "\n Title number " << i << ": ";
+			        cout << "Name: " << king_kind.name;
+			        cout << " - Alignment: " << king_kind.alignment;
+			        cout << " - Troop capacity when fighting: " << king_kind.troop_capacity;
+			        cout << " - Stats(*lvl): HP: " << king_kind.hp << " DMG: " << king_kind.dmg;
+			    }
+			    cout << "\nSelect the title number (anything other than the number to exit to civilization selection)\nYour choice: ";
+			    if(cin >> inputnumber){
+			        if((inputnumber >= 0) and (inputnumber < size)){
+			            king_selected = inputnumber;
+			            PlayerKingdom.king = king_selected;
+			        }
+			    }else{
+			        cin.clear();
+				    cin.ignore(10000, '\n');
+			    }
 			}else{
 				cout << "No option selected, choose again.\n";
 			}
