dbce7e2c49572dedd60448b1888f1d5763456751
Zurcaid <zurcaid.z@gmail.com>
2025-06-19 22:03:23 -0300

    Atualizar o RTSfunctions.cpp

--- RTS/RTSfunctions.cpp
+++ RTS/RTSfunctions.cpp
@@ -15,6 +15,7 @@ vector<vector<Champions>> commanders = {{MiyamotoMusashi, Spartacus}, {JonathanJ
 vector<vector<Troops>> soldier = {{Knight_1, Archer_1, Shielder_1, Horse_1}, {Cleric_1, HolyArcher_1, Paladin_1, Falcon_1}, {Goblin_1, RangedGoblin_1, Slime_1, Bat_1}, {Believer_1, ArcherBeliever_1, Penitent_1, ElementalSpirit_1}, {Summon_1, Mage_1, Caster_1, WolfSummon_1}, {Droid_1, RangedDroid_1, Barrier_1, Drone_1}, {Parasite_1, Bacteria_1, Flesh_1, ZombieWolf_1}};
 
 vector<vector<Troops>> medieval_troops = {{}, {}, {}, {}};
+vector<vector<Buildings>> medieval_buildings = {{}, {}, {}};
 
 vector<vector<Buildings>> constructions = {{}, {}, {}, {}, {}, {}, {}, {}};
 
@@ -33,25 +34,26 @@ Civilization::Civilization(int a, int b, int c, int d, int e = 1000)
 	alignment = kings.at(a).at(b).alignment;
 }
 
-void Civilization::setTroops(int x, int n)
+void Civilization::setTroops(int x, int k, int n)
 {
 	int size = troops.size();
 	int pos = size;
 	for (int i = 0; i < size; i++)
 	{
-		if (troops.at(i) == x)
+		if ((troops.at(i) == x) and (troops_kind.at(i) == k))
 		{
 			pos = i;
 			break;
 		}
 	}
-	if (pos <= size)
+	if (pos < size)
 	{
 		troops_num.at(pos) = troops_num.at(pos) + n;
 	}
 	else
 	{
 		troops.push_back(x);
+		troops.push_back(k);
 		troops_num.push_back(n);
 	}
 }
@@ -282,6 +284,8 @@ void setupTroops()
 			int size3 = sizeof(medieval_troops_list[i1][i2]) / sizeof(medieval_troops_list[i1][i2][0]);
 			for (int i3 = 0; i3 < size3; i3++)
 			{
+				if (medieval_troops_list[i1][i2][i3] == "0")
+					continue;
 				if (i1 == 0)
 				{
 					int hp = 10 + (2 * i3) + (20 * i2);
@@ -355,7 +359,7 @@ Army::Army(Champions leader, int leader_row, int moral, Troops row1, int row1_qn
 // Funcoes relativas ao gerenciamento de construcoes
 
 // t1=tech_req,nvl=lvl_required,a=kingdom,b=sector,c=type_id, nm=name, troop_type=troops;
-Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troop_type = 0, int troop_rm = 0)
+Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troop_kind = 0, int troop_type = 0, int troop_rm = 999)
 {
 	// Setor 1: Estracao de recursos da natureza.
 	// Setor 2: Refinamento ou utilizacao dos recursos extraidos.
@@ -366,11 +370,11 @@ Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troo
 	kingdom = a;
 	sector = b;
 	type_id = c;
-	int gen = lvl_req * tech_req;
+	int gen = lvl_req * tech_req * 10;
 	int qnt_rm = gen * (-1);
 	int worker = (gen / 10) + 2;
 	degradation_rate = 0.01 / tech_req;
-	if (tech_req >= 3)
+	if (tech_req >= 10)
 	{
 		steel_cost = tech_req * lvl_req * 0.9;
 		stone_cost = steel_cost * 2;
@@ -417,7 +421,7 @@ Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troo
 			chemicals = gen;
 			break;
 		case 4: // Industria armamentista
-			steel = qnt_rm;
+			steel = qnt_rm / 2;
 			gear = gen;
 			break;
 		case 5: // Industria de bens de consumo
@@ -425,7 +429,7 @@ Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troo
 			products = gen;
 			break;
 		case 6: // Institutos de pesquisa
-			chemicals = qnt_rm/2;
+			chemicals = qnt_rm / 2;
 			money = qnt_rm;
 			tech_gen = gen / 40;
 			break;
@@ -441,7 +445,7 @@ Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troo
 			break;
 		case 2: // Hospitais
 			money = qnt_rm;
-			chemicals -= qnt_rm/2;
+			chemicals -= qnt_rm / 2;
 			health = gen;
 			grow_rate = gen / 5;
 			break;
@@ -453,7 +457,10 @@ Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troo
 			criminal = worker * -3;
 			break;
 		case 5: // Academias para reinamento de soldados
-			troops_remove = troop_rm;
+			if (troops_remove != 999)
+				troops_remove = troop_rm;
+			gear -= qnt_rm;
+			troops_kind = troop_kind / 5;
 			troops = troop_type;
 			troops_num = worker;
 			worker = (troops_num / 10) + 1;
@@ -466,38 +473,42 @@ Buildings::Buildings(float t1, int nvl, int a, int b, int c, string nm, int troo
 	}
 }
 
-void Buildings::buildConstruction(Civilization &Obj)
+int Buildings::buildConstruction(Civilization &Obj)
 {
-	int food_necessity = Obj.population / ((Obj.food * 2) + Obj.raw_food + Obj.population);
-	int health_necessity = Obj.population / ((Obj.health * 4) + Obj.population);
-	Obj.money -= cost;
-	Obj.wood -= wood_cost;
-	Obj.stone -= stone_cost;
-	Obj.steel -= steel_cost;
-	Obj.worker += worker;
-	position += Obj.buildings.size();
-	Obj.buildings.push_back(*this);
-	if (((sector == 1) and (type_id == 1)) or ((sector == 2) and (type_id == 1)))
-		Obj.aproval += 20 * food_necessity;
-	if ((sector == 3) and (type_id == 2))
-		Obj.aproval += 15 * health_necessity;
+	if((Obj.money > cost) and (Obj.wood > wood_cost) and (Obj.stone > stone_cost) and (Obj.steel > steel_cost)){
+		float food_necessity = Obj.population / ((Obj.food * 2) + Obj.raw_food + Obj.population);
+		float health_necessity = Obj.population / ((Obj.health * 4) + Obj.population);
+		Obj.money -= cost;
+		Obj.wood -= wood_cost;
+		Obj.stone -= stone_cost;
+		Obj.steel -= steel_cost;
+		Obj.worker += worker;
+		position += Obj.buildings.size();
+		Obj.buildings.push_back(*this);
+		if (((sector == 1) and (type_id == 1)) or ((sector == 2) and (type_id == 1)))
+			Obj.aproval += 20 * food_necessity;
+		if ((sector == 3) and (type_id == 2))
+			Obj.aproval += 15 * health_necessity;
+		return 1;
+	}else{
+		return 0;
+	}
 }
 void Buildings::monthlyUpdate(Civilization &Obj)
 {
 	int storage;
 	integrity -= degradation_rate;
 	Obj.tech_lvl += tech_gen * integrity;
-	Obj.money += money * integrity;
-	Obj.gear += gear * integrity;
-	Obj.steel += steel * integrity;
-	Obj.paper += paper * integrity;
-	Obj.chemicals += chemicals * integrity;
-	Obj.food += food * integrity;
-	Obj.ores += ores * integrity;
-	Obj.wood += wood * integrity;
-	Obj.stone += stone * integrity;
-	Obj.raw_food += raw_food * integrity;
-	Obj.setTroops(troops, troops_num);
+	Obj.money += money * integrity * Obj.lvl;
+	Obj.gear += gear * integrity * Obj.lvl;
+	Obj.steel += steel * integrity * Obj.lvl;
+	Obj.chemicals += chemicals * integrity * Obj.lvl;
+	Obj.food += food * integrity * Obj.lvl;
+	Obj.ores += ores * integrity * Obj.lvl;
+	Obj.wood += wood * integrity * Obj.lvl;
+	Obj.stone += stone * integrity * Obj.lvl;
+	Obj.raw_food += raw_food * integrity * Obj.lvl;
+	Obj.setTroops(troops, troops_kind, (troops_num * Obj.lvl));
 
 	Obj.education += education / Obj.population;
 	if (Obj.education > 1)
@@ -516,24 +527,182 @@ void Buildings::monthlyUpdate(Civilization &Obj)
 	}
 }
 
+void setupBuildings()
+{
+	int size1 = 3;
+	int size2 = 6;
+	int size3 = 5;
+	for (int i1 = 0; i1 < size1; i1++)
+	{
+		for (int i2 = 0; i2 < size2; i2++)
+		{
+			for (int i3 = 0; i3 < size3; i3++)
+			{
+				int sector = i1 + 1;
+				int type_id = i2 + 1;
+				int lvl_req = 1 + (i3 * 20);
+				float tech_req = 1 + (i3 * 3);
+				string name = medieval_buildings_list[i1][i2][i3];
+				if (name == "0")
+					continue;
+				if ((i1 != 2) or (i2 != 4))
+				{
+					Buildings MedievalBuilding(tech_req, lvl_req, 0, sector, type_id, name);
+					medieval_buildings.at(i1).push_back(MedievalBuilding);
+				}
+				else
+				{
+					int size4 = 4;
+					for (int i4 = 0; i4 < size4; i4++)
+					{
+						int size5 = medieval_troops.at(i4).size();
+						for (int i5 = 0; i5 < size5; i5++)
+						{
+							tech_req = 1 + (i5 * (14.4 / size5));
+							lvl_req = 1;
+							int troop_rm = i5 - 1;
+							if ((i5 == 0) or (i4 == 3))
+							{
+								troop_rm = 999;
+							}
+							string tname = medieval_troops.at(i4).at(i5).name + name;
+							Buildings MedievalBuilding(tech_req, lvl_req, 0, sector, type_id, tname, i4, i5, troop_rm);
+							medieval_buildings.at(i1).push_back(MedievalBuilding);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 // Funcoes relativas ao gerenciamento de NPCs
 BotIA::BotIA(Civilization &Obj1, King &Obj2)
 {
 	BotCivilization = &Obj1;
 	BotKing = &Obj2;
 }
+
+void BotIA::botBuild()
+{
+	int population = BotCivilization->population;
+	int lvl = BotCivilization->lvl;
+
+	float food_necessity = population / ((BotCivilization->food * 2) + BotCivilization->raw_food + population);
+	float health_necessity = population / ((BotCivilization->health * 4) + population);
+	float tech_necessity = (lvl * 4) / BotCivilization->tech_lvl;
+	float chem_necessity = ((tech_necessity + health_necessity) * BotCivilization->tech_lvl * lvl) / BotCivilization->chemicals;
+
+	int size = BotCivilization->troops.size();
+	float war_power = 0.0;
+	for (int i = 0; i < size; i++)
+	{
+		int pos = BotCivilization->troops.at(i);
+		int kind = BotCivilization->troops_kind.at(i);
+		float troop_power = (medieval_troops.at(kind).at(pos).hp * medieval_troops.at(kind).at(pos).dmg) / 100;
+		war_power += troop_power * BotCivilization->troops_num.at(i);
+	}
+
+	float troop_necessity = (population / lvl) / (war_power);
+	float gear_necessity = ((troop_necessity*lvl*BotCivilization->tech_lvl)/2) / BotCivilization->gear;
+	float cost_necessity = food_necessity + health_necessity + troop_necessity + gear_necessity;
+	float stone_necessity = (cost_necessity*2*lvl) / BotCivilization->stone;
+	float wood_necessity;
+	float steel_necessity;
+	if(BotCivilization->tech_lvl <= 10){
+		wood_necessity = (cost_necessity * 2 * lvl) / BotCivilization->wood;
+		steel_necessity = ((gear_necessity * lvl * BotCivilization->tech_lvl) / 2) / BotCivilization->steel;
+	}else{
+		wood_necessity = ((cost_necessity-troop_necessity)-gear_necessity) / BotCivilization->wood;
+		steel_necessity = (((cost_necessity * lvl * BotCivilization->tech_lvl) + (gear_necessity * lvl * BotCivilization->tech_lvl) / 2)) / BotCivilization->steel;
+	}
+	cost_necessity += stone_necessity + steel_necessity + wood_necessity;
+	float money_necessity = (cost_necessity*lvl*BotCivilization->tech_lvl) / BotCivilization->money;
+	
+	vector<float> necessities = {food_necessity, health_necessity, tech_necessity, chem_necessity, troop_necessity, gear_necessity, stone_necessity, wood_necessity, steel_necessity, money_necessity};
+	size = necessities.size();
+	vector<int> order;
+	for(int i1 = 0; i1 < size; i1++){
+		int necessity = necessities.at(i1);
+		if (i1 == 0)
+		{
+			order.push_back(necessity);
+		}
+		else
+		{
+			int size1 = order.size();
+			for (int i2 = 0; i2 < size1; i2++)
+			{
+				if (necessity > order.at(i2))
+				{
+					order.insert(order.begin() + i2, necessity);
+					break;
+				}
+			}
+		}
+	}
+	for(int i1 = 0; i1 < 3; i1++){
+		int unemployed = (population - BotCivilization->worker) - BotCivilization->criminal;
+		int build = 0;
+		int necessity = order.at(i1);
+		switch(necessity){
+			case 0:
+				for(int i2 = 4; i2 >= 0; i2 -= 1){
+					Buildings* actual_building = &medieval_buildings.at(1).at(i2);
+					if(BotCivilization->raw_food > actual_building->raw_food){
+						if(unemployed > actual_building->worker)
+							build = actual_building->buildConstruction(*BotCivilization);
+					}else{
+						actual_building = &medieval_buildings.at(0).at(i2);
+						if(unemployed > actual_building->worker)
+							build = actual_building->buildConstruction(*BotCivilization);
+					}
+					if(build == 1)
+						break;
+				}
+				break;
+			case 1:
+				for(int i2 = 9; i2 > 4; i2 -= 1){
+					Buildings* actual_building = &medieval_buildings.at(2).at(i2);
+					if(unemployed > actual_building->worker)
+						build = actual_building->buildConstruction(*BotCivilization)
+				}
+				break;
+			case 2:
+			
+				break;
+			case 3:
+			
+				break;
+			case 4:
+			
+				break;
+			case 5:
+				
+				break;
+			case 6:
+			
+				break;
+			case 7:
+				
+				break;
+			case 8:
+				
+				break;
+			case 9:
+				
+				break;
+		}
+	}
+}
+
 void BotIA::botTurn()
 {
 	// Fase de exploracao
 	int random1 = rand() % 4 + 1;
 	BotCivilization->exploreDirection(random1);
-	int size = constructions.size();
-	for (int i = 0; i < size; i++)
-	{
-		if (BotCivilization->tech_lvl)
-		{
-		}
-	}
+
+	// Fase de construcao
 }
 
 // Funcoes relativas ao andamento do jogo
@@ -542,6 +711,8 @@ Civilization PlayerKingdom(0, 0, 0, 0);
 
 int beginGame()
 {
+	setupTroops();
+	setupBuildings();
 	cout << end_of_page;
 	cout << "Choose how do you want to proceed:\n1 - New game.\n2 - Load game.\nYour choice: ";
 	cin >> input1;
@@ -596,140 +767,4 @@ void startNewPlayer()
 		cin >> input1;
 		cout << "Are you sure your character's name is: '" << input1 << "' ?\n1 - Yes.\n2 - No.\nYour choice: ";
 		cin >> input2;
-		if (input2 == "1")
-		{
-			PlayerKingdom.emperor_name = input1;
-		}
-	}
-	while (PlayerKingdom.civilization_name == "")
-	{
-		cout << end_of_page;
-		cout << "Digit the name of your empire: ";
-		cin >> input1;
-		cout << "Are you sure the your empire's name is: '" << input1 << "' ?\n1 - Yes.\n2 - No.\nYour choice: ";
-		cin >> input2;
-		if (input2 == "1")
-		{
-			PlayerKingdom.civilization_name = input1;
-		}
-	}
-	int king_selected = 100;
-	int kind_selected;
-	int inputnumber;
-	if (style == 2)
-	{
-		while (king_selected == 100)
-		{
-			cout << end_of_page;
-			cout << "Choose a civilization kind:\n0 - Human\n1 - Holy\n2 - Evil\n3 - Deity\n4 - Witchcraft\n5 - Technology\n6 - Biotechnology\nChoose one to see the types of king you can select: ";
-			while (!(cin >> kind_selected))
-			{
-				cout << "No option selected, choose again.\n";
-				cout << end_of_page;
-				cout << "Choose a civilization kind:\n0 - Human\n1 - Holy\n2 - Evil\n3 - Deity\n4 - Witchcraft\n5 - Technology\n6 - Biotechnology\nChoose one to see the types of king you can select: ";
-				cin.clear();
-				cin.ignore(10000, '\n');
-			}
-			if ((kind_selected >= 0) and (kind_selected <= 6))
-			{
-				vector<King> kingdom_kind = kings.at(kind_selected);
-				int size = kingdom_kind.size();
-				cout << "Available title options available for this kind of kingdom:\n";
-				for (int i = 0; i < size; i++)
-				{
-					King king_kind = kingdom_kind.at(i);
-					cout << "\n Title number " << i << ": ";
-					cout << "Name: " << king_kind.name;
-					cout << " - Alignment: " << king_kind.alignment;
-					cout << " - Troop capacity when fighting: " << king_kind.troop_capacity;
-					cout << " - Stats(*lvl): HP: " << king_kind.hp << " DMG: " << king_kind.dmg;
-				}
-				cout << "\nSelect the title number (anything other than the number to exit to civilization selection)\nYour choice: ";
-				if (cin >> inputnumber)
-				{
-					if ((inputnumber >= 0) and (inputnumber < size))
-					{
-						king_selected = inputnumber;
-						PlayerKingdom.king = king_selected;
-						PlayerKingdom.kind = kind_selected;
-						PlayerKingdom.defineEmperor();
-					}
-				}
-				else
-				{
-					cin.clear();
-					cin.ignore(10000, '\n');
-				}
-			}
-			else
-			{
-				cout << "No option selected, choose again.\n";
-			}
-		}
-	}
-}
-
-void generateOtherCivilizations()
-{
-	int count = 0;
-	int size1 = kings.size(); // Quantidade de estilos de civilizacao
-	int possible_kinds[7][4];
-	for (int i1 = 0; i1 < size1; i1++)
-	{
-		int size2 = kings.at(i1).size();
-		for (int i2 = 0; i2 < size2; i2++)
-		{
-			possible_kinds[i1][i2] = 1;
-		}
-	}
-	possible_kinds[PlayerKingdom.kind][PlayerKingdom.king] = 0;
-	if (style == 1)
-		size1 = 1;
-	for (int i1 = 0; i1 < size1; i1++)
-	{
-		int size2 = kings.at(i1).size();
-		for (int i2 = 0; i2 < size2; i2++)
-		{
-			if (possible_kinds[i1][i2] != 1)
-			{
-				continue;
-			}
-			if (difficulty == 1)
-			{
-				int x = (rand() % 10000) - 5000;
-				while (x == 0)
-				{
-					x = (rand() % 10000) - 5000;
-				}
-				int y = (rand() % 3000) - 1500;
-				Civilization KingdomNPC(i1, i2, x, y, count);
-				KingdomNPC.defineEmperor();
-				botCivilizations.push_back(KingdomNPC);
-				count++;
-			}
-		}
-	}
-	int size = botCivilizations.size();
-	for (int i = 0; i < size; i++)
-	{
-		botCivilizations.at(i).setNationsUnknown();
-	}
-}
-
-void startNPCS()
-{
-}
-
-void newGame()
-{
-	startNewPlayer();
-	generateOtherCivilizations();
-}
-
-void loadGame()
-{
-}
-
-void playerTurn()
-{
-}
+		if (input2
